8ff76c9cddb0404a0b4a6a41cd4072e75708c404
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-14 14:17:58 -0300

    5º requisito completado

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ function eraseLetter() {
   const letterContainer = document.getElementById('carta-gerada');
   letterContainer.innerText = '';
 }
+/*
+* Recorri ao seguinte site pro entendimento da função test().
+* Link: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/using-the-test-method
+*/
+
+function isInvalidInput(txtInput) {
+  const testRegex = /^\s+/;
+  return testRegex.test(txtInput);
+}
 
 function createLetter() {
   eraseLetter();
   const txtLetter = document.getElementById('carta-texto').value;
   const letterContainer = document.getElementById('carta-gerada');
-  const wordSplited = txtLetter.split(' ');
-  for (let index = 0; index < wordSplited.length; index += 1) {
-    const wordSpan = document.createElement('span');
-    wordSpan.innerText = wordSplited[index];
-    letterContainer.appendChild(wordSpan);
+  if (!isInvalidInput(txtLetter)) {
+    const wordSplited = txtLetter.split(' ');
+    for (let index = 0; index < wordSplited.length; index += 1) {
+      const wordSpan = document.createElement('span');
+      wordSpan.innerText = wordSplited[index];
+      letterContainer.appendChild(wordSpan);
+    }
+  } else {
+    letterContainer.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
